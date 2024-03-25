fe00f4746bce51e0417eec82812e687e4e3c9919
Buddha <buddhalama88480@gmail.com>
2024-03-25 07:21:57 -0700

    added pages and components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Login from "./pages/login/login"
+import HeroSection from "./components/heroSection/heroSection"
+
+function App() {
+
+
+  return (
+    <>
+      <HeroSection/>
+      <Login/>
+  
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,8 @@
+import styles from "./login.module.css"
+export default function Login(){
+    return(
+        <>
+        <h1>This is login page</h1>
+        </>
+    )
+}
